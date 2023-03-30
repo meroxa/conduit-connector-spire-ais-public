2155a94177b20a52d8383f6eb908a914e9551def
William Hill <william@meroxa.io>
2023-03-30 16:51:10 -0400

    fix: removed unused variable to fix linting error

--- spec.go
+++ spec.go
@@ -18,10 +18,6 @@ import (
 	sdk "github.com/conduitio/conduit-connector-sdk"
 )
 
-// version is set during the build process with ldflags (see Makefile).
-// Default version matches default from runtime/debug.
-var version = "(devel)"
-
 // Specification returns the connector's specification.
 func Specification() sdk.Specification {
 	return sdk.Specification{
